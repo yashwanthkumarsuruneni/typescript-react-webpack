b9df790235d96c2d8a3c3fb8ea545c1f2eec47ee
Yashwanth kumar Suruneni <yashksuruneni@Yashwanths-MBP.fios-router.home>
2018-11-18 10:52:21 -0500

    es5

--- config/webpack.production.ts
+++ config/webpack.production.ts
@@ -1,18 +1,48 @@
+import * as webpack from 'webpack';
 import * as webpackMerge from 'webpack-merge';
 import * as path from 'path';
 import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import * as OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+// import * as UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 import common  from './webpack.common';
 
 
-const production = webpackMerge(common, {
+const production : webpack.Configuration  = webpackMerge(common, {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, '../dist')
     },
+    optimization: {
+    //   runtimeChunk: 'single',
+    //   minimizer: [
+    //       new UglifyJsPlugin({
+    //         cache: true,
+    //         extractComments: false,
+    //         parallel: true,
+    //         uglifyOptions: {
+    //             compress: {
+    //               inline: true
+    //             }
+    //         },
+    //         sourceMap: false
+    //       })
+    //   ],
+      splitChunks: {
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
+              chunks: 'all'
+            }
+          }
+      }
+    },
     plugins: [
     new MiniCssExtractPlugin({
        filename: '[name].[contenthash].css',   
+    }), 
+    new OptimizeCssAssetsPlugin({
     })
     ]
 });
